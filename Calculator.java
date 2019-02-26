b1e534b010fd248ad72d3f990714420fc47f5101
andreilarsen <42559253+andreilarsen@users.noreply.github.com>
2019-02-25 21:27:32 -0700

    Part 2

--- Calculator.java
+++ Calculator.java
@@ -5,14 +5,16 @@ package cse360assign2;
  * addition, subtraction, multiplication, and division.
  * @author Andrei Larsen
  */
-public class Calculator {
+public class Calculator
+{
 
 	private int total;
 	
 	/**
 	 * Constructor
 	 */
-	public Calculator () {
+	public Calculator ()
+	{
 		total = 0;  // not needed - included for clarity
 	}
 	
@@ -20,32 +22,36 @@ public class Calculator {
 	 * Returns the total.
 	 * @return the total
 	 */
-	public int getTotal () {
-		return 0;
+	public int getTotal ()
+	{
+		return total;
 	}
 	
 	/**
 	 * Adds the total with the parameter.
 	 * @param value the parameter to add
 	 */
-	public void add (int value) {
-		
+	public void add (int value)
+	{
+		total += value;
 	}
 	
 	/**
 	 * Subtracts the parameter from the total.
 	 * @param value the parameter to subtract
 	 */
-	public void subtract (int value) {
-		
+	public void subtract (int value)
+	{
+		total -= value;
 	}
 	
 	/**
 	 * Multiplies the total by the parameter.
 	 * @param value the parameter by which to multiply
 	 */
-	public void multiply (int value) {
-		
+	public void multiply (int value)
+	{
+		total *= value;
 	}
 	
 	/**
@@ -53,8 +59,12 @@ public class Calculator {
 	 * Sets the result to zero if the parameter is zero.
 	 * @param value the parameter by which to divide
 	 */
-	public void divide (int value) {
-		
+	public void divide (int value)
+	{
+		if(value == 0)
+			total = 0;
+		else
+			total /= value;
 	}
 	
 	/**
@@ -62,7 +72,8 @@ public class Calculator {
 	 * have been performed on this calculator object.
 	 * @return A string representing the history
 	 */
-	public String getHistory () {
+	public String getHistory ()
+	{
 		return "";
 	}
 }
